652e44c5375b3b812e6712a9f0478ccc52b5dc2f
Pablo Carceller <pablocarceller@me.com>
2020-03-04 13:31:26 +0100

    LogIn Works

--- server/app.js
+++ server/app.js
@@ -1,85 +1,22 @@
-require('dotenv').config();
+// Enviroment variables
+require('dotenv').config()
 
-const bodyParser   = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+// Database connection
+require('./configs/mongoose.config')
 
-const session    = require("express-session");
-const MongoStore = require('connect-mongo')(session);
-const flash      = require("connect-flash");
-    
+// Application instance
+const express = require('express')
+const app = express()
 
-mongoose
-  .connect('mongodb://localhost/server', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
-
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
-
-const app = express();
-
-// Middleware Setup
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-// Express View engine setup
-
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-      
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+// Configs
+require('./configs/middleware.config')(app)
+require('./configs/locals.config')(app)
+require('./configs/session.config')(app)
 
 
-hbs.registerHelper('ifUndefined', (value, options) => {
-  if (arguments.length < 2)
-      throw new Error("Handlebars Helper ifUndefined needs 1 parameter");
-  if (typeof value !== undefined ) {
-      return options.inverse(this);
-  } else {
-      return options.fn(this);
-  }
-});
-  
+// Base URLS
+app.use('/api/auth', require('./routes/auth.routes'))
+app.use('/api/coasters', require('./routes/coasters.routes'))
+app.use('/api/files', require('./routes/files.routes.js'))
 
-// default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
-
-
-// Enable authentication using session + passport
-app.use(session({
-  secret: 'irongenerator',
-  resave: true,
-  saveUninitialized: true,
-  store: new MongoStore( { mongooseConnection: mongoose.connection })
-}))
-app.use(flash());
-require('./passport')(app);
-    
-
-const index = require('./routes/index');
-app.use('/', index);
-
-const authRoutes = require('./routes/auth');
-app.use('/auth', authRoutes);
-      
-
-module.exports = app;
+module.exports = app
